5aa8562c453681fbf159d5415d6a5e11a4c3680b
Drey <drey.ast25@gmail.com>
2023-04-27 10:40:42 -0300

    added css

--- imc.js
+++ imc.js
@@ -13,13 +13,14 @@ var peso;
 var imc;
 var resultado;
 
-function calcular(event){
+function calcular(event) {
     event.preventDefault();
-    
+
     peso = parseFloat(document.getElementById("peso").value);
     altura = parseFloat(document.getElementById("altura").value);
 
     imc = peso / (altura * altura);
+    console.log(imc)
 
     resultado = document.getElementById("resultado");
 
@@ -27,9 +28,9 @@ function calcular(event){
         resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Cuidado, você está muito abaixo do peso!";
     } else if (imc >= 17 && imc <= 18.49) {
         resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Abaixo do peso!";
-    } else if (imc >= 18.5 && imc <=24.99) {
+    } else if (imc >= 18.5 && imc <= 24.99) {
         resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está no peso ideal!";
-    } else if (imc >= 25 && imc <= 29.99){
+    } else if (imc >= 25 && imc <= 29.99) {
         resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Você está com sobrepeso!";
     } else if (imc >= 30 && imc <= 34.99) {
         resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> CUIDADO, obesidade grau I!";
@@ -39,6 +40,6 @@ function calcular(event){
         resultado.innerHTML = "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> CUIDADO, obesidade grau III!";
     }
 
-document.getElementById("peso").value = "";
-document.getElementById("altura").value = "";
+    document.getElementById("peso").value = "";
+    document.getElementById("altura").value = "";
 }
